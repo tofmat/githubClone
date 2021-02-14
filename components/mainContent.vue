fdd87d250ccafb80687ca7b4a62b2e781a8caaf7
tofmat <tofmatt@gmail.com>
2021-02-14 19:36:38 +0100

    done with left hand side

---
+++ components/mainContent.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="content">
+    <div class="lhs px-15">
+      <div class="lhImage">
+        <a href="">
+          <img src="@/assets/me.jpeg" alt="profile">
+          <div class="babyFace">
+            <svg class="octicon octicon-smiley" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z"></path></svg>
+          </div>
+        </a>
+      </div>
+      <div class="userInfo">
+        <h1>Tosin Ogunfowote</h1>
+        <h2>tofmat</h2>
+        <h2 class="de">Frontend Developer</h2>
+        <div>
+          <button>
+            Edit Profile
+          </button>
+        </div>
+        <div class="flex foll items-center">
+          <div class="mr-5 flex items-center">
+            <svg class="octicon octicon-people text-gray-light mr-5" height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M5.5 3.5a2 2 0 100 4 2 2 0 000-4zM2 5.5a3.5 3.5 0 115.898 2.549 5.507 5.507 0 013.034 4.084.75.75 0 11-1.482.235 4.001 4.001 0 00-7.9 0 .75.75 0 01-1.482-.236A5.507 5.507 0 013.102 8.05 3.49 3.49 0 012 5.5zM11 4a.75.75 0 100 1.5 1.5 1.5 0 01.666 2.844.75.75 0 00-.416.672v.352a.75.75 0 00.574.73c1.2.289 2.162 1.2 2.522 2.372a.75.75 0 101.434-.44 5.01 5.01 0 00-2.56-3.012A3 3 0 0011 4z"></path></svg>
+            <a href="" class="cli">
+              <span class="mr-5">11</span>followers
+            </a>
+          </div>
+          <span class="mr-5"> . </span>
+          <div class="mr-5">
+            <a href="" class="flex items-center cli">
+              <span class="mr-5" >4</span>following
+            </a>
+          </div>
+          <span class="mr-5"> . </span>
+          <div class="flex items-center">
+            <svg class="octicon octicon-star text-gray-light mr-5" height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>
+            <a href="" class="cli">
+              <span class="mr-5">15</span>
+            </a>
+          </div>
+        </div>
+        <div class="details">
+          <div class="flex items-center mx-10">
+            <svg class="octicon octicon-mail" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.75 2A1.75 1.75 0 000 3.75v.736a.75.75 0 000 .027v7.737C0 13.216.784 14 1.75 14h12.5A1.75 1.75 0 0016 12.25v-8.5A1.75 1.75 0 0014.25 2H1.75zM14.5 4.07v-.32a.25.25 0 00-.25-.25H1.75a.25.25 0 00-.25.25v.32L8 7.88l6.5-3.81zm-13 1.74v6.441c0 .138.112.25.25.25h12.5a.25.25 0 00.25-.25V5.809L8.38 9.397a.75.75 0 01-.76 0L1.5 5.809z"></path></svg>
+            tofmatt@gmail.com
+          </div>
+          <div class="flex items-center mx-10">
+            <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg>
+            tosinogunfowote.com
+          </div>
+          <div class="flex items-center mx-10">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 273.5 222.3" class="octicon"><path d="M273.5 26.3a109.77 109.77 0 0 1-32.2 8.8 56.07 56.07 0 0 0 24.7-31 113.39 113.39 0 0 1-35.7 13.6 56.1 56.1 0 0 0-97 38.4 54 54 0 0 0 1.5 12.8A159.68 159.68 0 0 1 19.1 10.3a56.12 56.12 0 0 0 17.4 74.9 56.06 56.06 0 0 1-25.4-7v.7a56.11 56.11 0 0 0 45 55 55.65 55.65 0 0 1-14.8 2 62.39 62.39 0 0 1-10.6-1 56.24 56.24 0 0 0 52.4 39 112.87 112.87 0 0 1-69.7 24 119 119 0 0 1-13.4-.8 158.83 158.83 0 0 0 86 25.2c103.2 0 159.6-85.5 159.6-159.6 0-2.4-.1-4.9-.2-7.3a114.25 114.25 0 0 0 28.1-29.1" fill="currentColor"></path></svg>
+            @dtofmat
+          </div>
+        </div>
+        <div class="details">
+          <h3>Highlights</h3>
+          <div class="flex items-center mx-10">
+            <svg class="octicon octicon-mail" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.75 2A1.75 1.75 0 000 3.75v.736a.75.75 0 000 .027v7.737C0 13.216.784 14 1.75 14h12.5A1.75 1.75 0 0016 12.25v-8.5A1.75 1.75 0 0014.25 2H1.75zM14.5 4.07v-.32a.25.25 0 00-.25-.25H1.75a.25.25 0 00-.25.25v.32L8 7.88l6.5-3.81zm-13 1.74v6.441c0 .138.112.25.25.25h12.5a.25.25 0 00.25-.25V5.809L8.38 9.397a.75.75 0 01-.76 0L1.5 5.809z"></path></svg>
+            Arctic Code Vault Contributor
+          </div>
+          <div class="flex items-center mx-10">
+            <svg class="octicon octicon-cpu text-gray-light mr-1" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M6.5.75a.75.75 0 00-1.5 0V2H3.75A1.75 1.75 0 002 3.75V5H.75a.75.75 0 000 1.5H2v3H.75a.75.75 0 000 1.5H2v1.25c0 .966.784 1.75 1.75 1.75H5v1.25a.75.75 0 001.5 0V14h3v1.25a.75.75 0 001.5 0V14h1.25A1.75 1.75 0 0014 12.25V11h1.25a.75.75 0 000-1.5H14v-3h1.25a.75.75 0 000-1.5H14V3.75A1.75 1.75 0 0012.25 2H11V.75a.75.75 0 00-1.5 0V2h-3V.75zm5.75 11.75h-8.5a.25.25 0 01-.25-.25v-8.5a.25.25 0 01.25-.25h8.5a.25.25 0 01.25.25v8.5a.25.25 0 01-.25.25zM5.75 5a.75.75 0 00-.75.75v4.5c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-4.5a.75.75 0 00-.75-.75h-4.5zm.75 4.5v-3h3v3h-3z"></path></svg>
+            Developer Program Member
+          </div>
+        </div>
+        <div class="details nobottomBorder">
+          <h3>Organizations</h3>
+          <div class="flex items-center mx-10">
+            <img src="@/assets/hendray.png" alt="hendray" width="32px" height="32px">
+            <img src="@/assets/airsynq.png" alt="airsynq" width="32px" height="32px">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.content{
+  padding: 0 8%;
+}
+.lhs{
+  width: 25%;
+  margin-top: -30px;
+}
+.lhImage{
+  position: relative;
+}
+.lhImage img {
+  border-radius: 50%!important;
+  width: 100%;
+  height: 280px;
+}
+.babyFace svg{
+  border: solid #b5b9be 0.1px;
+  padding: 10px;
+  border-radius: 50%;
+  background-color: white;
+  fill: #24292e;
+}
+.babyFace {
+  position: absolute;
+  bottom: 0;
+  left: 100%;
+  z-index: 2;
+  margin-bottom: 32px;
+  margin-left: -60px;
+}
+.userInfo h1{
+  font-size: 26px;
+  line-height: 1.25;
+  color: #24292e;
+  margin-bottom: 1px;
+}
+.userInfo h2{
+  font-style: normal;
+  font-weight: 300;
+  color: #586069;
+  margin-top: 0 !important;
+}
+.de{
+  color: #24292e !important;
+  font-weight: 400 !important;
+  margin-bottom: 20px;
+}
+.userInfo button {
+  width: 100%;
+  border: solid rgba(27,31,35,0.15) 1px;
+  padding: 5px 10px;
+  color: #24292e;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  border-radius: 6px;
+  background-color: #fafbfc;
+  margin-bottom: 20px;
+}
+.foll svg {
+  fill: #6a737d;
+}
+.foll span{
+  color: black;
+}
+.foll{
+  color: #6a737d;
+  font-size: 14px;
+}
+.foll a {
+  text-decoration: none;
+  color: #6a737d;
+}
+.cli :hover{
+  color: #0366d6 !important;
+}
+.details{
+  margin: 20px 0;
+  padding-bottom: 20px;
+  font-size: 14px;
+  color: #24292e;
+  border-bottom: #b5b9be solid 1px;
+}
+.details svg {
+  margin-right: 10px;
+}
+.nobottomBorder{
+  border-bottom: none;
+}
+</style>
